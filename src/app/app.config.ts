bad8b5900ee021912b815e0751991b4c3973208a
karmasakshi <karmasakshi@users.noreply.github.com>
2024-02-10 20:47:27 +0400

    feat(general): add transloco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@ngneat/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })]
+    }), provideHttpClient(), provideTransloco({
+        config: {
+          availableLangs: ['ar', 'en'],
+          defaultLang: 'en',
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })]
 };
