105f06d77811d7cd41f900624af4c2cadf954fc7
karmasakshi <karmasakshi@users.noreply.github.com>
2024-02-10 20:22:59 +0400

    refactor(general): replace app component with main component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'xxx-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
