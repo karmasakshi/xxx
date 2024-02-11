ec9bd2e31790743c093829e243c6128aea96665d
karmasakshi <karmasakshi@users.noreply.github.com>
2024-02-11 23:35:50 +0400

    feat(general): generate language menu component

---
+++ src/app/components/language-menu/language-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xxx-language-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-menu.component.html',
+  styleUrl: './language-menu.component.scss',
+})
+export class LanguageMenuComponent {}
