d1558b9cf6ae118965564fa2932cbdcdad24471f
karmasakshi <karmasakshi@users.noreply.github.com>
2024-02-17 16:41:51 +0400

    feat(general): add main component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,58 +7,60 @@ import { MatListModule } from '@angular/material/list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
+import { Language } from '@xxx/interfaces/language.interface';
 import { LoaderConfiguration } from '@xxx/interfaces/loader-configuration.interface';
 import { LoaderService } from '@xxx/services/loader/loader.service';
 import { LoggerService } from '@xxx/services/logger/logger.service';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { LanguageMenuComponent } from '../language-menu/language-menu.component';
-import { TranslocoModule } from '@ngneat/transloco';
-import { CommonModule } from '@angular/common';
-import { Language } from '@xxx/interfaces/language.interface';
 import { SettingsService } from '@xxx/services/settings/settings.service';
+import { Observable } from 'rxjs';
+import { LanguageMenuComponent } from '../language-menu/language-menu.component';
+import { APP_NAME } from '@xxx/constants/app-name.constant';
 
 @Component({
   selector: 'xxx-main',
   standalone: true,
   imports: [
-    LanguageMenuComponent,
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
     CommonModule,
-    TranslocoModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
     MatProgressBarModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    RouterModule,
+    TranslocoModule,
+    LanguageMenuComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  public appName: string;
   public directionality: Language['directionality'];
-  public isSmallViewport$: Observable<boolean>;
+  public isSmallViewport: boolean;
   public loaderConfiguration$: Observable<LoaderConfiguration>;
 
   public constructor(
     private readonly _breakpointObserver: BreakpointObserver,
-    private readonly _loggerService: LoggerService,
     private readonly _loaderService: LoaderService,
+    private readonly _loggerService: LoggerService,
     private readonly _settingsService: SettingsService,
   ) {
+    this.appName = APP_NAME;
+
     this.directionality =
       this._settingsService.settings.language.directionality;
 
-    this.loaderConfiguration$ = this._loaderService.loaderConfiguration$;
+    this.isSmallViewport = this._breakpointObserver.isMatched([
+      Breakpoints.Handset,
+      Breakpoints.Tablet,
+    ]);
 
-    this.isSmallViewport$ = this._breakpointObserver
-      .observe([Breakpoints.Handset, Breakpoints.Tablet])
-      .pipe(
-        map((result) => result.matches),
-        shareReplay(),
-      );
+    this.loaderConfiguration$ = this._loaderService.loaderConfiguration$;
 
     this._loggerService.logComponentInitialization('MainComponent');
   }
