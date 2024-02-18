2dac77bbf4fa35400ecd69a3db0c27ea485eac11
karmasakshi <karmasakshi@users.noreply.github.com>
2024-02-18 09:04:47 +0400

    feat(general): generate help page

---
+++ src/app/components/help-page/help-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xxx-help-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './help-page.component.html',
+  styleUrl: './help-page.component.scss',
+})
+export class HelpPageComponent {}
